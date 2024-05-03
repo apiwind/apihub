73e05dab1883dfc660fdf4d9e17b8d2cb5471e5c
Your Name <your-email@example.com>
2024-05-03 21:11:52 +0800

    build

--- build/push.sh
+++ build/push.sh
@@ -3,5 +3,5 @@
 git add .
 git checkout -b "build"
 git commit -m "Deploy changes from dev to main"
-ecgo $APIHUB_GITHUB_TOKEN
+echo $APIHUB_GITHUB_TOKEN
 git push --force https://thlz998:$APIHUB_GITHUB_TOKEN@github.com/apiwind/apihub build
